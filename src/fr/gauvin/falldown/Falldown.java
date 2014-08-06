2571f33e41d241e025fa9a7c055895e77e729ba4
odarkriseo <gauvin.alexandre.35@gmail.com>
2014-08-06 23:19:18 +0200

    hello world swing

---
+++ src/fr/gauvin/falldown/Falldown.java
@@ -0,0 +1,43 @@
+package fr.gauvin.falldown;
+
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Falldown extends JFrame {
+
+	public Falldown() {
+		super("Falldown");
+
+		WindowListener l = new WindowAdapter() {
+			public void windowClosing(WindowEvent e){
+				System.exit(0);
+			}
+		};
+		
+		//JButton b = new JButton("toto");
+		
+		Canvas c = new Canvas() {
+			@Override
+			public void paint(Graphics g) {
+				super.paint(g);
+				
+				g.drawLine(10, 10, 20, 20);
+			}
+		};
+		
+		c.setSize(200, 100);
+		
+		add(c);
+
+		addWindowListener(l);
+		setSize(200,100);
+		setVisible(true);
+	}
+
+}
---
+++ src/fr/gauvin/falldown/Main.java
@@ -0,0 +1,11 @@
+package fr.gauvin.falldown;
+
+public class Main {
+	
+
+	public static void main(String[] args) {
+		new Falldown();
+
+	}
+
+}
